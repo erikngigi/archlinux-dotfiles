91d5ec96d6b03f2965adbb7c192077b315585600
erikngigi <ericmosesngigi@gmail.com>
2026-02-07 17:57:48 +0300

    feat: change polybar script

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -2,13 +2,13 @@
 
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
+# If all your bars have ipc enabled, you can also use
 # polybar-msg cmd quit
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar eric-1 &
+polybar lenny1 &
 
 if [[ $(xrandr -q | grep 'DP2 connected') ]]; then
-  polybar eric-2 &
+  polybar lenny2 &
 fi
